e7c4ab124c2e32f51eaabb0b8085ecce8ef5fdd2
george-g <georgegombert@gmail.com>
2020-04-27 10:02:04 -0600

    Avatar link working. License and version badges working.

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const github = require('octonode'); //https://github.com/pksunkara/octonode reco
 const writeFile = util.promisify(fs.writeFile);
 
 
-function genarateReadMe(readMe){
+function genarateReadMe(readMe) {
   const file = `## ${readMe.name}
+  ![Version](${readMe.version})
   ****
   ### Description
   ${readMe.description}
@@ -26,8 +27,8 @@ function genarateReadMe(readMe){
   ${readMe.usage}
   ### Badges
   ${readMe.badges}
-  ### License
-  ${readMe.licence}
+  ### License ![License](${readMe.licence})
+  
   ### Contributing
   ${readMe.contributing}
   ### Tests
@@ -36,9 +37,25 @@ function genarateReadMe(readMe){
   ${readMe.questions}
   ### Project Status
   ${readMe.status}`
-  console.log('hit');
+  // console.log('hit');
   return file;
 } // end genarateReadMe
+
+function githubInfo(id) {
+  const client = github.client();
+  return new Promise((resolve, reject) => {
+    client.get('/users/' + id + '', {},
+      function (err, status, body, headers) {
+        if (err) {
+          reject(err);
+        }
+        else{
+          resolve(body.avatar_url); //json object
+        }
+      }
+    );
+  })
+} // end githubInfo
 
 function getUsername() {
   return inquirer
@@ -50,116 +67,118 @@ function getUsername() {
 } //end getUsername
 
 function getProjectName() {
-    return inquirer
-      .prompt([{
-        type: "input",
-        message: "What is the name of your project?",
-        name: "name"
-      }])
-  } //end getProjectName
+  return inquirer
+    .prompt([{
+      type: "input",
+      message: "What is the name of your project?",
+      name: "name"
+    }])
+} //end getProjectName
 
-  function getDescritpion() {
-    return inquirer
-      .prompt([{
-        type: "input",
-        message: "Write a brief description of your project",
-        name: "description"
-      }])
-  } //end getDescription
+function getDescritpion() {
+  return inquirer
+    .prompt([{
+      type: "input",
+      message: "Write a brief description of your project",
+      name: "description"
+    }])
+} //end getDescription
 
-  function getInstallation() {
-    return inquirer
-      .prompt([{
-        type: "input",
-        message: "Write installation instructions",
-        name: "install"
-      }])
-  } //end getInstall
+function getInstallation() {
+  return inquirer
+    .prompt([{
+      type: "input",
+      message: "Write installation instructions",
+      name: "install"
+    }])
+} //end getInstall
 
-  function getUsage() {
-    return inquirer
-      .prompt([{
-        type: "input",
-        message: "How is your app used",
-        name: "usage"
-      }])
-  } //end getUsage
+function getUsage() {
+  return inquirer
+    .prompt([{
+      type: "input",
+      message: "How is your app used",
+      name: "usage"
+    }])
+} //end getUsage
 
-  function getBadges() {
-    return inquirer
-      .prompt([{
-        type: "input",
-        message: "What badges would you like to include?",
-        name: "badges"
-      }])
-  } //end getBadges
+function getVersion() {
+  return inquirer
+    .prompt([{
+      type: "input",
+      message: "What version of your project is this?",
+      name: "version"
+    }])
+} //end getVersion
 
-  function getLicense() {
-    return inquirer
-      .prompt([{
-        type: "input",
-        message: "What licencing is this project under?",
-        name: "licence"
-      }])
-  } //end getLicence
+function getLicense() {
+  return inquirer
+    .prompt([{
+      type: "rawlist",
+      message: "What licencing is this project under?",
+      name: "licence",
+      choices: ["Apache", "MIT", "BSD", "W3C", "GPL", "LGPL", "MPL", "EPL", "Other"]
+    }])
+} //end getLicence
 
-  function getContributing() {
-    return inquirer
-      .prompt([{
-        type: "input",
-        message: "who are the major contributors to the project",
-        name: "contributing"
-      }])
-  } //end getContributing
+function getContributing() {
+  return inquirer
+    .prompt([{
+      type: "input",
+      message: "who are the major contributors to the project",
+      name: "contributing"
+    }])
+} //end getContributing
 
-  function getTests() {
-    return inquirer
-      .prompt([{
-        type: "input",
-        message: "Are there any tests that can be run on this project?",
-        name: "tests"
-      }])
-  } //end getTests
+function getTests() {
+  return inquirer
+    .prompt([{
+      type: "input",
+      message: "Are there any tests that can be run on this project?",
+      name: "tests"
+    }])
+} //end getTests
 
-  function getQuestions() {
-    return inquirer
-      .prompt([{
-        type: "input",
-        message: "If someone has a question, how do they contact you?",
-        name: "questions"
-      }])
-  } //end getQuestions
+function getQuestions() {
+  return inquirer
+    .prompt([{
+      type: "input",
+      message: "If someone has a question, how do they contact you?",
+      name: "questions"
+    }])
+} //end getQuestions
 
-  function getStatus() {
-    return inquirer
-      .prompt([{
-        type: "input",
-        message: "What is the current status of the project and are there any plans for the future?",
-        name: "status"
-      }])
-  } //end getStatus
+function getStatus() {
+  return inquirer
+    .prompt([{
+      type: "input",
+      message: "What is the current status of the project and are there any plans for the future?",
+      name: "status"
+    }])
+} //end getStatus
 
 
-async function init(){
+async function init() {
   const readMe = {
-    id : (await getUsername()).id,
+    id: (await getUsername()).id,
+    photo: await (githubInfo(this.id)),
     name : (await getProjectName()).name,
     description : (await getDescritpion()).description,
     install : (await getInstallation()).install,
     usage : (await getUsage()).usage,
-    badges : (await getBadges()).badges,
-    licence : (await getLicense()).licence,
+    version : `https://img.shields.io/badge/Version-${(await getVersion()).version}-green`,
+    licence : `https://img.shields.io/badge/License-${(await getLicense()).licence}-blue`,
     contributing: (await getContributing()).contributing,
     tests : (await getTests()).tests,
     questions : (await getQuestions()).questions,
     status : (await getStatus()).status,
   };
-
+  
 
   console.log(readMe);
 
   writeFile("readme2.md", genarateReadMe(readMe))
-    .then(() => console.log("file created successfully with promisify!"))
+    .then(() => console.log("file created successfully!"))
     .catch(error => console.log(error));
 };
 
